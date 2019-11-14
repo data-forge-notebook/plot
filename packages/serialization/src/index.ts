33ac664cd3ef0eda0469fee486fe9208015c93d5
Ashley Davis <ashley@codecapers.com.au>
2019-11-14 19:20:21 +1000

    Added serialization interface. Moved the plot library to the plot library to the packages directory.

---
+++ packages/serialization/src/index.ts
@@ -0,0 +1,36 @@
+
+/**
+ * A serialized data series.
+ */
+export interface IDataSeries {
+
+    /**
+     * The data type of the series.
+     */
+    type: string;
+
+    /**
+     * Values in the data data series.
+     */
+    values: any[];
+}
+
+/**
+ * A map of named data series.
+ */
+export interface IDataSeriesMap {
+    [index: string]: IDataSeries;
+}
+
+/**
+ * The serialized form of a data. 
+ * This is an ordinary JavaScript data structure that can be used to transfer a data across the wire and
+ * reinstantiate it on the otherside.
+ */
+export interface ISerializedData {
+
+    /**
+     * Collection of named data series.
+     */
+    series: IDataSeriesMap;
+}
