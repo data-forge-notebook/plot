542c40b05b91385d45ce3208fe06b392ea9bcf11
Ashley Davis <ashley@codecapers.com.au>
2020-10-09 15:25:27 +1000

    Added option to disable animations.

--- packages/lib-apex/src/index.ts
+++ packages/lib-apex/src/index.ts
@@ -67,6 +67,12 @@ export interface IMountOptions {
     makeStatic?: boolean;
 
     /**
+     * Set to false to disble animations in charts.
+     * Defaults to true.
+     */
+    enableAnimations?: boolean;
+
+    /**
      * Debug log the chart definition after formatting.
      */
     showChartDef?: boolean;
@@ -108,6 +114,10 @@ export async function mountChart(chartDef: IChartDef, domElement: HTMLElement, c
         apexChartDef.tooltip!.enabled = true;
         apexChartDef.chart!.zoom!.enabled = true;
         apexChartDef.chart!.toolbar!.show = true;
+    }
+
+    if (chartOptions && chartOptions.enableAnimations === false) {
+        apexChartDef.chart!.animations!.enabled = false;
     }
 
     if (chartOptions && chartOptions.showChartDef) {
