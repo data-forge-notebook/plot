479998d7ee8cc7a00b26a336d61c9b42d444cfa4
Ashley Davis <ashley@codecapers.com.au>
2021-01-18 18:33:52 +1000

    New statically built docs page.

---
+++ docs/42da9be5.ff00ff9a.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[8],{77:function(o){o.exports=JSON.parse('{"allTagsPath":"/blog/tags","slug":"facebook","name":"facebook","count":1,"permalink":"/blog/tags/facebook"}')}}]);
