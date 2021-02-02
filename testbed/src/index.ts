bcd0288d5d57b32ab27f8125b46421360c9f27b3
Ashley Davis <ashley@codecapers.com.au>
2021-02-03 09:00:40 +1000

    Expanded the testbed to be able to test the full API.

--- testbed/src/index.ts
+++ testbed/src/index.ts
@@ -1,12 +1,17 @@
+import { plot } from "plot";
+import "@plotex/render-dom"
 import { mountChart } from "@plotex/lib-apex";
 
 async function main(): Promise<void> {
 
-    const response = await fetch("chart-def.json");
-    const chartData = await response.json();
+    const plotDef = await fetch("plot-def.json").then(response => response.json());
+    const plt = plot(plotDef.data,, plotDef.plotConfig, plotDef.axisMap); 
+    plt.renderDOM(document.getElementById("chart1")!);
+
+    const chartData = await fetch("full-chart-def.json").then(response => response.json());
     console.log("Chart def: ");
     console.log(chartData);
-    await mountChart(chartData.chartDef, document.getElementById("chart")!, chartData.options);
+    await mountChart(chartData.chartDef, document.getElementById("chart2")!, chartData.options);
 }
 
 main()
